70c081c077e5d921fc5caebd8a42f8250065e7a0
Mateusz Grzonka <alpinus4@gmail.com>
2022-11-07 21:59:57 +0100

    Add data generation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
-from random import randint, uniform
+from random import randint, uniform, choice
 from datetime import date, timedelta
-from copy import deepcopy
 
 from faker import Faker
 from faker_food import FoodProvider
@@ -15,10 +14,10 @@ def daterange(start_date, end_date):
 
 
 class OrderSQL:
-    def __init__(self, order_id):
+    def __init__(self, order_id, phone_nb):
         self.order_id = order_id
         self.creation_date = fake.date()
-        self.phone_nb = fake.phone_number()
+        self.phone_nb = phone_nb
 
 
 class OrdersItemSQL:
@@ -41,7 +40,7 @@ class AdSQL:
         self.ad_id = ad_id
         self.cost = round(uniform(2000, 100000), 2)
         self.start_date = fake.date_between_dates(date_start=date(2021, 1, 1), date_end=date(2021, 12, 31))
-        self.end_date = fake.date_time_between_dates(date_start=self.start_date, date_end=date(2021, 12, 31))
+        self.end_date = fake.date_between_dates(date_start=self.start_date, date_end=date(2021, 12, 31))
 
 
 class PromotionSQL:
@@ -49,13 +48,13 @@ class PromotionSQL:
         self.promotion_id = promotion_id
         self.worth = randint(10, 90)
         self.start_date = fake.date_between_dates(date_start=date(2021, 1, 1), date_end=date(2021, 12, 31))
-        self.end_date = fake.date_time_between_dates(date_start=self.start_date, date_end=date(2021, 12, 31))
+        self.end_date = fake.date_between_dates(date_start=self.start_date, date_end=date(2021, 12, 31))
         self.dish_id = dish_id
 
 
 class UserXML:
-    def __init__(self, phone_nb):
-        self.phone_nb = phone_nb
+    def __init__(self):
+        self.phone_nb = fake.phone_number()
         self.name = fake.first_name()
         self.surname = fake.last_name()
 
@@ -74,27 +73,40 @@ class AdXML:
 
 
 ORDERS_COUNT = 1000
-DISH_COUNT = AD_COUNT = 100
+AD_COUNT = 100
+USER_COUNT = 5000
 
-# these classes are independent from others
-orders = []
-ads = []
-for id in range(ORDERS_COUNT):
-    orders.append(OrderSQL(id))
+users = []
+for _ in range(USER_COUNT):
+    users.append(UserXML())
 
+ads_sql = []
 for id in range(AD_COUNT):
-    ads.append(AdSQL(id))
+    ads_sql.append(AdSQL(id))
 
-# DishSQL
-# UserXML
-# AdXML
-
-# OrdersItemSQL
-# PromotionSQL
+orders = []
+for id in range(ORDERS_COUNT):
+    orders.append(OrderSQL(id, choice(users).phone_nb))
 
 dishes = []
-ads_copy = deepcopy(ads)
-for id in range(DISH_COUNT):
-    ad = random.choice(ads_copy)
-    ads_copy.remove(ad)
-    dishes.append(DishSQL(id, ad.ad_id))
+dish_id = 0
+for ad in ads_sql:
+    for _ in range(randint(1, 3)):
+        dishes.append(DishSQL(dish_id, ad.ad_id))
+        dish_id += 1
+
+ads_xml = []
+for ad_sql in ads_sql:
+    ads_xml.append(AdXML(ad_sql.ad_id, ad_sql.start_date, ad_sql.end_date))
+
+orders_items = []
+for order in orders:
+    for _ in range(randint(1, 5)):
+        orders_items.append(OrdersItemSQL(choice(dishes).dish_id, order.order_id))
+
+promotions = []
+promotion_id = 0
+for dish in dishes:
+    for _ in range(randint(1, 3)):
+        promotions.append(PromotionSQL(promotion_id, dish.dish_id))
+        promotion_id += 1
