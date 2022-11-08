ccb16940902112306152d1c9079ffe7945481a78
Mateusz Grzonka <alpinus4@gmail.com>
2022-11-08 20:51:57 +0100

    Fix dish generation to prevent duplicates

--- src/dish.py
+++ src/dish.py
@@ -3,7 +3,7 @@ import random
 class Dish:
     def __init__(self, fake, dish_id, ad_id):
         self.dish_id = dish_id
-        self.name = fake.dish()
+        self.name = fake.dish() + " with " + random.choice([fake.ingredient(), fake.vegetable()]).lower()
         self.price = round(random.uniform(20, 80), 2)
         self.ad_id = ad_id
 
